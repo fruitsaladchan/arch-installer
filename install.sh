d190dd1c12fd183af1f4e8a512ff73b05ad98bf4
fruit salad <124645742+fruitsaladchan@users.noreply.github.com>
2025-01-20 14:42:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -815,7 +815,7 @@ echo -ne "
            ║                Installing extra packages           ║
            ╚════════════════════════════════════════════════════╝
 "
-pacman -S --noconfirm --needed fd fzf ripgrep sd neovim eza bat net-tools fastfetch btop htop xdg-user-dirs bash-completion dmidecode jq util-linux
+pacman -S --noconfirm --needed fd fzf ripgrep sd neovim eza bat net-tools fastfetch btop htop xdg-user-dirs bash-completion dmidecode jq util-linux go rust python-pip
 echo "  installing usefull tools"
 xdg-user-dirs-update
 curl -o /home/$USERNAME/.bashrc https://raw.githubusercontent.com/fruitsaladchan/bashrc/refs/heads/main/.bashrc
